14c1dac424098dca20a10ab64339dc20addea754
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-06-23 18:56:05 +0530

    Friend Request backend finished

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import "dotenv/config"
 import authRoutes from  "./routes/auth.route.js"
 import { connectDB } from './lib/db.js'
+import userRoutes from "./routes/user.route.js"
 import cookieParser from "cookie-parser"
-
+import chatRoutes from "../src/routes/chat.route.js"
 
 const app = express()
 const PORT = process.env.PORT
@@ -11,6 +12,8 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes)
 
 app.listen(5001,() =>{
     console.log(`Server is running on port ${PORT}`)
