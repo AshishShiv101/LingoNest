64adec0a31c999f8cf7da25484a8ffefab9a4c6e
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-06-29 17:10:46 +0530

    Notifications Page completed

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,19 +1,26 @@
-import express from 'express'
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from '../controllers/user.controller.js';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+    acceptFriendRequest,
+    getFriendRequests,
+    getMyFriends,
+    getOutgoingFriendReqs,
+    getRecommendedUsers,
+    sendFriendRequest,
+} from "../controllers/user.controller.js";
+
 const router = express.Router();
 
+// apply auth middleware to all routes
+router.use(protectRoute);
 
-router.use(protectRoute)
+router.get("/", getRecommendedUsers);
+router.get("/friends", getMyFriends);
 
-router.get("/",getRecommendedUsers)
-router.get("/friends", getMyFriends)
 router.post("/friend-request/:id", sendFriendRequest);
-router.post("/friend-request/:id/accept", acceptFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-router.post("/friend-request", getFriendRequests);
-
-router.get("/outgoing-friend-requests",getOutgoingFriendReqs)
-
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -47,7 +47,7 @@ const HomePage = () => {
       setOutgoingRequestsIds(outgoingIds);
     }
   }, [outgoingFriendReqs]);
-
+      
   return (
     <div className="p-4 sm:p-6 lg:p-8">
       <div className="container mx-auto space-y-10">
