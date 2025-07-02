39ff3fbf8f11ed149a7e533a5f58dfb1a35f6231
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-07-02 22:51:38 +0530

    Project Finalised for production

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import "dotenv/config"
 import path from "path";
-import authRoutes from  "./routes/auth.route.js"
-import { connectDB } from './lib/db.js'
-import userRoutes from "./routes/user.route.js"
+import authRoutes from  "./src/routes/auth.route.js"
+import { connectDB } from './src/lib/db.js'
+import userRoutes from "./src/routes/user.route.js"
 import cookieParser from "cookie-parser"
-import chatRoutes from "../src/routes/chat.route.js"
+import chatRoutes from "./src/routes/chat.route.js"
 import cors from "cors"
 
 const app = express()
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../")));
 
     app.get("*",(req,res) => {
         res.sendFile( path.join(__dirname,"../frontend","dist","index.html"))
