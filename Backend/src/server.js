e41e9deedc0be06fb45f3f8a4e9e74d9ef8a0a94
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-07-02 22:16:05 +0530

    Project Finalised for production

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import "dotenv/config"
+import path from "path";
 import authRoutes from  "./routes/auth.route.js"
 import { connectDB } from './lib/db.js'
 import userRoutes from "./routes/user.route.js"
@@ -10,7 +11,9 @@ import cors from "cors"
 const app = express()
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -21,6 +24,14 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile( path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(5001,() =>{
     console.log(`Server is running on port ${PORT}`)
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,53 +4,53 @@ import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
 
-const Navbar = () => {  
+const Navbar = () => {
     const { authUser } = useAuthUser();
     const location = useLocation();
     const isChatPage = location.pathname?.startsWith("/chat");
-
     const { logoutMutation } = useLogout();
 
     return (
         <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-end w-full">
-                    {/* LOGO - ONLY IN THE CHAT PAGE */}
-                    {isChatPage && (
-                        <div className="pl-5">
-                            <Link to="/" className="flex items-center gap-2.5">
-                                <ShipWheelIcon className="size-9 text-primary" />
-                                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                                    Streamify
-                                </span>
-                            </Link>
-                        </div>
-                    )}
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8 w-full">
+                <div className="flex items-center justify-between w-full">
+
+                    {/* SITE NAME (Always visible on mobile, hidden if Sidebar is visible) */}
+                    <div className="flex items-center gap-2.5 lg:hidden">
+                        <Link to="/" className="flex items-center gap-2.5">
+                            <ShipWheelIcon className="size-8 text-primary" />
+                            <span className="text-2xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wide">
+                                LingoNest
+                            </span>
+                        </Link>
+                    </div>
 
                     <div className="flex items-center gap-3 sm:gap-4 ml-auto">
-                        <Link to={"/notifications"}>
+                        <Link to="/notifications">
                             <button className="btn btn-ghost btn-circle">
                                 <BellIcon className="h-6 w-6 text-base-content opacity-70" />
                             </button>
                         </Link>
-                    </div>
 
-                    {/* TODO */}
-                    <ThemeSelector />
+                        <ThemeSelector />
 
-                    <div className="avatar">
-                        <div className="w-9 rounded-full">
-                            <img src={authUser?.profilePic} alt="User Avatar" rel="noreferrer" />
+                        <div className="avatar">
+                            <div className="w-9 rounded-full">
+                                <img src={authUser?.profilePic} alt="User Avatar" />
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Logout button */}
-                    <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
-                        <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
-                    </button>
+                        <button
+                            className="btn btn-ghost btn-circle"
+                            onClick={logoutMutation}
+                        >
+                            <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </nav>
     );
 };
+
 export default Navbar;
